a6640b246d05223d64ae68ccdc25fa75b81f2050
Jian Chen <quantumsix492@gmail.com>
2013-05-04 23:44:30 -0700

    first commit

---
+++ havefunblog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Havefunblog::Application.config.secret_token = 'f9fa5aa57f0e1da3f76163cc7eeee3631e7de5c930e8c9b2092e2cf5c71c8175d76b063c32ee1ef7b5ce2c3ad8adc591e9ee20a1fc83e08443a5b6ad4ba634a5'
